8660eac305275e405288747e616ff16d30529381
Jintao Koong <jintaokoong99@gmail.com>
2021-08-07 15:18:16 +0800

    feat: initial commit
    - commit with initial app layout

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Main = styled.main`
+  border-left: 0;
+  border-right: 0;
+  
+  @media (min-width: 770px) {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 414px;
+    
+    border-left: 1px solid rgba(0, 0, 0, 0.15);
+    border-right: 1px solid rgba(0, 0, 0, 0.15);
+  }
+
+  min-height: 100vh;
+`;
+
+function App() {
+  return (
+    <Main className={'App'}>
+
+    </Main>
+  );
+}
+
+export default App;
